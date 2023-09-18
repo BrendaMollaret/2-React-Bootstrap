867e81e3a2108f8c942f9ab991690eea15c2594c
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-09-18 19:13:55 -0300

    Created base

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import CarouselHome from "./components/CarouselHome/CarouselHome"
+import Header from "./components/Navbar/Header"
+
+function App () {
+  return (
+    <>
+
+     <Header />
+     <CarouselHome/>
+      
+
+    </>
+
+  )
+}
+
+export default App
