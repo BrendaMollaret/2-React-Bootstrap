6c92c261e4aa31186b8cc92f1f739ca488e7dada
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-09-19 12:54:05 -0300

    Landing page terminada

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import AboutUs from "./components/AboutUs/AboutUs"
 import CarouselHome from "./components/CarouselHome/CarouselHome"
+import Footer from "./components/Footer/Footer"
 import Header from "./components/Navbar/Header"
 
 function App () {
@@ -7,7 +9,8 @@ function App () {
 
      <Header />
      <CarouselHome/>
-      
+     <AboutUs/>
+     <Footer/>
 
     </>
 
